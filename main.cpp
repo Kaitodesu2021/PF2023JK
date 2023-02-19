526ece7dace9e653d2a741dbd37af3c5b745d3aa
MUHAMMAD ALIF BIN KHABALI <1211103373@student.mmu.edu.my>
2023-02-19 20:33:59 +0800

    finalise

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // Course: TCP1101 PROGRAMMING FUNDAMENTALS 
 // Year: Trimester 1, 2022/23 (T2215) 
 // Lab: TT5L 
-// Names: ALIF | FARID | 
+// Names: MUHAMMAD ALIF BIN KHABALI | MUHAMMAD FARID BIN JAYATAN | 
 // IDs: 1211103373 | 1211103085 | 
 // Emails: 1211103373@student.mmu.edu.my | 1211103085@student.mmu.edu.my | 
 // Phones: 0174622108 | 01120975650 | 
